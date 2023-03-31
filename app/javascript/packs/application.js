134305fd8a173cc4849efc4fc096943f8468252d
taku-zfof <taku.uchida.zfof@gmail.com>
2023-03-31 20:58:17 +0000

    ratyのturbolinks無効化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,9 +11,10 @@ import "jquery"
 import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
 import Raty from "raty.js"
-window.raty = function(elem,opt) {
+
+window.raty = 'turbolinks:load',function(elem,opt) {
   let raty =  new Raty(elem,opt)
   raty.init();
   return raty;
